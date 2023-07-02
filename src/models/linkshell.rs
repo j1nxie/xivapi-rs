707886e483f55523e2c9c3784817a710b4997248
j1nxie <rylieeeeexd@gmail.com>
2023-07-02 19:08:23 +0700

    feat: match LinkshellResult with current results

--- src/models/linkshell.rs
+++ src/models/linkshell.rs
@@ -1,15 +1,26 @@
-use super::{search::character::SearchCharacter, LodestoneInfo};
+use super::{id::LinkshellId, search::Pagination, Member};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct LinkshellResult {
-    pub info: Info,
-    pub linkshell: Vec<SearchCharacter>,
+    pub linkshell: Option<Linkshell>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct Info {
-    pub linkshell: LodestoneInfo,
+pub struct Linkshell {
+    #[serde(rename = "ID")]
+    pub id: LinkshellId,
+    pub pagination: Pagination,
+    pub profile: LinkshellProfile,
+    #[serde(rename = "Results")]
+    pub members: Vec<Member>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct LinkshellProfile {
+    pub name: String,
+    pub server: String,
 }
