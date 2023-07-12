2f6ea1f3605a8d4023e54aa60e4d759de80687a3
j1nxie <rylieeeeexd@gmail.com>
2023-07-12 20:12:11 +0700

    fix(Search/Achievement): comment out unavailable fields
    these fields seem to only be available when you fetch the achievement by its ID? commented out instead of removing so that i could look further into this.

--- src/models/search/achievement.rs
+++ src/models/search/achievement.rs
@@ -6,9 +6,9 @@ use serde::Deserialize;
 pub struct Achievement {
     #[serde(flatten)]
     pub metadata: Metadata,
-    pub points: u64,
-    #[serde(rename = "AchievementCategory.Name")]
-    pub achievement_category_name: String,
-    #[serde(rename = "AchievementCategory.AchievementKind.Name")]
-    pub achievement_category_achievement_kind_name: String,
+    // pub points: u64,
+    // #[serde(rename = "AchievementCategory.Name")]
+    // pub achievement_category_name: String,
+    // #[serde(rename = "AchievementCategory.AchievementKind.Name")]
+    // pub achievement_category_achievement_kind_name: String,
 }
